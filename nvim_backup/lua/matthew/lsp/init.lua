ee37620403bb1f05b6bea00b54d95ac4bd610384
Matthew Burfield <m1.burfield@gmail.com>
2023-02-28 16:12:47 +1000

    remove backup nvim

--- nvim_backup/lua/matthew/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-	return
-end
-
-require("matthew.lsp.mason")
-require("matthew.lsp.lsp-installer")
-require("matthew.lsp.handlers").setup()
--- nvim_backup/lua/matthew/package-info.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, packageInfo = pcall(require, "package-info")
-if not status_ok then
-	return
-end
-
-packageInfo.setup({})
