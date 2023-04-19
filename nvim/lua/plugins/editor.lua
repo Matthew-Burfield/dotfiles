6440c324a58802cd8519362e006920fb9e27b74a
Matthew Burfield <Matthew-Burfield@users.noreply.github.com>
2023-04-19 15:37:28 +1000

    feat: update nvim-tree mappings

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,3 +1,15 @@
+local function on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  -- local function opts(desc)
+  --   return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  -- end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set("n", "s", "", { buffer = bufnr })
+end
+
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   {
@@ -11,9 +23,8 @@ return {
       { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle Nvim Tree" },
     },
     opts = function()
-      local config = require("nvim-tree.config")
-      local cb = config.nvim_tree_callback
       return {
+        on_attach = on_attach,
         actions = {
           open_file = {
             quit_on_open = true,
@@ -25,15 +36,6 @@ return {
         update_focused_file = {
           enable = true,
           update_cwd = true,
-        },
-        view = {
-          mappings = {
-            list = {
-              { key = { "l", "<CR>", "o" }, cb = cb("edit") },
-              { key = "h", cb = cb("close_node") },
-              { key = "v", cb = cb("vsplit") },
-            },
-          },
         },
         renderer = {
           root_folder_modifier = ":t",
