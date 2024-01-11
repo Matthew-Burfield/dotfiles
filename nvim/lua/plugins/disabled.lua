99468539b78c9f074c5776854226fef3c74fee3e
Matthew Burfield <m1.burfield@gmail.com>
2024-01-12 06:55:50 +1000

    added tmux

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,7 +1,6 @@
 return {
   -- { "folke/trouble.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
